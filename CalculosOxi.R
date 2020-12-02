7c86da2341e85477f6b96ce6fde1756775f6a00f
Renato Bastos Pope <renato@pope.med.br>
2020-12-02 18:22:43 -0300

    foi

--- CalculosOxi.R
+++ CalculosOxi.R
@@ -1,7 +1,6 @@
+install.packages("dplyr")
 df <- cirurgias
 head(df)
 tabela = table(df$Oxigenador , df$Desfecho)
 tabela
 summary(tabela)
-tabelaB = table(Braile$Oxigenador, Braile$Desfecho)
-summary(tabelaB)
